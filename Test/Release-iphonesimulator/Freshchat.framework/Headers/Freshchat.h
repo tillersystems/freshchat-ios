2efa0900bad6604252732dd9c13ffea7b82dd779
benoit munoz <benoit.munoz@sumup.com>
2021-10-06 00:25:16 +0200

    Test SPM

---
+++ Test/Release-iphonesimulator/Freshchat.framework/Headers/Freshchat.h
@@ -0,0 +1,18 @@
+//
+//  Freshchat.h
+//  Freshchat
+//
+//  Created by Benoit MUNOZ on 06/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Freshchat.
+FOUNDATION_EXPORT double FreshchatVersionNumber;
+
+//! Project version string for Freshchat.
+FOUNDATION_EXPORT const unsigned char FreshchatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Freshchat/PublicHeader.h>
+
+
